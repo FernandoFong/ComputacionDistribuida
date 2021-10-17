60be628bb8876a79029ec2c3b6f214be7a69bbbd
Esjag García <marioagd@ciencias.unam.mx>
2021-10-17 02:03:59 -0500

    Se modifico el módulo 4.
    Se modificarón las salidas para ser más legibles, además se agregaron comentarios, ejemplos de uso.
    
    La funcionalidad de la recepción de mensajes también se módifico, ahora lee todos los mensajes en su buzón.
    
    Práctica 1
    Módulo: 4
    Autor: Mario Angel García Domínguez
    Fecha: 16 de octubre de 2021
    Para la asignatura Computación Distribuida 2022-1.

--- Practica1/Practica1.ex
+++ Practica1/Practica1.ex
@@ -230,132 +230,382 @@ end
 
 defmodule Module4 do
 
+  @doc """
+    Crea un proceso, llamado server que va a inicializar todas las estructuras
+    de datos. Va a guardar el ID de este proceso, ya que va a ser elemental pa-
+    ra establecer un canal de comunicación con el proceso que resuelve las es-
+    tructuras.
+
+    Este proceso principal, solo va a mandar mensajes al servidor, luego,
+    abrirá y consumirá su buzón con la función auxiliar, buzon/1.
+  """
   def monstructure() do
-    pid = spawn(fn -> server end)
-    #send(pid, {:delete, :list, self(), 3})
-    #send(pid, {:put, :list, self(), 6})
-    #send(pid, {:length, :list, self()})
-    #send(pid, {:algo, :list, self()})
-    #send(pid, {:get, :tuple, self()})
-    #send(pid, {:set, :tuple, self(), :b, 1})
-    #send(pid, {:to_list, :tuple, self()})
-    #send(pid, {:algo, :tuple, self()})
-    #send(pid, {:put, :map, self(), :l, 3})
-    #send(pid, {:get_value, :map, self(), :d})
-    #send(pid, {:eval_lamb, :map, self(), :a, fn x, y -> x + y end, 5})
-    #send(pid, {:algo, :map, self()})
-    #send(pid, {:in, :map_set, self(), 10})
-    #send(pid, {:size, :map_set, self()})
-    #send(pid, {:put, :map_set, self(), 15})
-    send(pid, {:algo, :map_set, self()})
-    receive do
-      {:ok, :delete, :list} ->
-        "Se elimino correctamente el elemento de la lista."
-      {:ok, :put, :list} ->
-        "Se agrego el elemento a la lista."
-      {:ok, :length, :list, length} ->
-        "El tamaño de la lista es #{inspect length}."
-      {:ok, :get, :tuple, t} ->
-        "La tupla es #{inspect t}"
-      {:ok, :set, :tuple} ->
-        "La tupla se actualizó correctamente."
-      {:ok, :to_list, :tuple, tl} ->
-        tl
-      {:ok, :put, :map} ->
-        "Se agrego al diccionario correctamente la llave y el valor."
-      {:ok, :get_value, :map, value} ->
-        "El elemento asociado con la llave es #{inspect value}."
-      {:ok, :eval_lamb, :map, value} ->
-        "El resultado de la operación es #{inspect value}."
-      {:ok, :in, :map_set, i} ->
-        "Es #{inspect i} que el elemento esta en el conjunto."
-      {:ok, :size, :map_set, size} ->
-        "El tamaño del conjunto es #{inspect size}."
-      {:ok, :put, :map_set} ->
-        "Se agrego el elemento correctamente."
-      {:error, :op_invalida} ->
-        :op_error
-      {:error, :not_found} ->
-        :not_found_error
-    after
-      1_000 -> "Se rebaso el tiempo de espera."
+    # En este caso se implemento una función recursiva que mantiene
+    # al canal ecuchando, por eso en cada llamada, debe de mandar las
+    # estrcutrudas de datos.
+
+    # Ninguna operación agrega elementos a la tupla, por lo que para el
+    # ejemplo, esta no se inicializa en vacío.
+    pid = spawn(fn -> server([], {:a, 2, "a", 3, :b}, Map.new, MapSet.new, true) end)
+
+    # Secuencias de ejemplos para cada estructura.
+    #ejemplos_listas(pid)
+    #ejemplos_tuplas(pid)
+    #ejemplos_diccionarios(pid)
+    ejemplos_conjuntos(pid)
+
+    # Llama al buzón.
+    buzon(true)
+  end
+
+  @doc """
+    Función con una secuencia de uso para listas en esta monstruo estrcutura.
+  """
+  defp ejemplos_listas(pid) do
+    # Ejemplos de peticiones a listas.
+    send(pid, {:add, :list, self(), 1})
+    send(pid, {:add, :list, self(), 2})
+    send(pid, {:add, :list, self(), 3})
+    send(pid, {:add, :list, self(), 4})
+    send(pid, {:add, :list, self(), 5})
+    send(pid, {:length, :list, self()})
+    send(pid, {:delete, :list, self(), 3})
+    send(pid, {:length, :list, self()})
+    send(pid, {:add, :list, self(), 6})
+    send(pid, {:add, :list, self(), 7})
+    send(pid, {:length, :list, self()})
+    # Estas dos no son reconocidas por el servidor.
+    send(pid, {:concat, :list, self(), [1,2,3]})
+    send(pid, {:size, :list, self()})
+  end
+
+  @doc """
+    Función con una secuencia de uso para tuplas en esta monstruo estrcutura.
+  """
+  defp ejemplos_tuplas(pid) do
+    # Ejemplos de peticiones a tuplas.
+    send(pid, {:get, :tuple, self()})
+    send(pid, {:set, :tuple, self(), :c, 0})
+    send(pid, {:set, :tuple, self(), 1, 1})
+    send(pid, {:set, :tuple, self(), "b", 2})
+    send(pid, {:set, :tuple, self(), :d, 4})
+    send(pid, {:get, :tuple, self()})
+    send(pid, {:to_list, :tuple, self()})
+    # Estas dos no son reconocidas por el servidor.
+    send(pid, {:equal, :tuple, self(), {1,2,3,:a,"b"}})
+    send(pid, {:firs_elem, :tuple, self()})
+  end
+
+  @doc """
+    Función con una secuencia de uso para diccionarios en esta monstruo estrcutura.
+  """
+  defp ejemplos_diccionarios(pid) do
+    # Ejemplos de peticiones a diccionarios.
+    send(pid, {:put, :map, self(), :a, 1})
+    send(pid, {:put, :map, self(), :b, 2})
+    send(pid, {:put, :map, self(), :c, 3})
+    send(pid, {:get_value, :map, self(), :a})
+    send(pid, {:get_value, :map, self(), :b})
+    send(pid, {:get_value, :map, self(), :d})
+    send(pid, {:eval_lamb, :map, self(), :a, fn x, y -> x + y end, 5})
+    send(pid, {:eval_lamb, :map, self(), :b, fn x, y -> x * y end, 10})
+    # Estas dos no son reconocidas por el servidor.
+    send(pid, {:get_key, :map, self(), 2})
+    send(pid, {:set_elements, :map, self(), {:b, 1}})
+  end
+
+  @doc """
+    Función con una secuencia de uso para conjuntos en esta monstruo estrcutura.
+  """
+  defp ejemplos_conjuntos(pid) do
+    # Ejemplo de peticiones a conjuntos.
+    send(pid, {:put, :map_set, self(), 1})
+    send(pid, {:put, :map_set, self(), 2})
+    send(pid, {:put, :map_set, self(), 3})
+    send(pid, {:put, :map_set, self(), 4})
+    send(pid, {:size, :map_set, self()})
+    send(pid, {:put, :map_set, self(), 5})
+    send(pid, {:put, :map_set, self(), 6})
+    send(pid, {:put, :map_set, self(), 1})
+    send(pid, {:size, :map_set, self()})
+    send(pid, {:in, :map_set, self(), 1})
+    send(pid, {:in, :map_set, self(), 10})
+    # Estas dos no son reconocidas por el servidor.
+    send(pid, {:union, :map_set, self(), MapSet.new([10,11,12])})
+    send(pid, {:elemnts, :map_set, self()})
+  end
+
+  @doc """
+    Función que va a leer el buzón de entrada del proceso principal, hasta
+    que deje de recibir mensajes. Cuando su buzón este vació reportará que
+    ha terminado de leer.
+
+    Para detectar cuando no hay más mensajes se usa un tiempo de espera de
+    1 segundo, y cambia la bandera que recibe la función.
+  """
+  defp buzon(bandera) do
+    if bandera do
+      receive do
+        # Listas.
+        {:ok, :delete, :list, elem} ->
+          IO.puts("Se elimino correctamente #{inspect elem} de la lista.")
+          buzon(true)
+        {:ok, :add, :list, elem} ->
+          IO.puts("Se agrego #{inspect elem} a la lista.")
+          buzon(true)
+        {:ok, :length, :list, length} ->
+          IO.puts("El tamaño de la lista es #{inspect length}.")
+          buzon(true)
+
+          # Tuplas.
+        {:ok, :get, :tuple, tuple} ->
+          IO.puts("La tupla es #{inspect tuple}")
+          buzon(true)
+        {:ok, :set, :tuple, value, pos} ->
+          IO.puts("Se modifico el valor de la tupla en la posición #{inspect pos}, ahora es #{inspect value}")
+          buzon(true)
+        {:ok, :to_list, :tuple, list} ->
+          IO.puts("La tupla como lista es #{inspect list}.")
+          buzon(true)
+
+          # Diccionarios
+        {:ok, :put, :map, key, value} ->
+          IO.puts("Se agrego al diccionario el valor #{inspect value} con la llave #{inspect key}.")
+          buzon(true)
+        {:ok, :get_value, :map, key, value} ->
+          IO.puts("El elemento con la llave #{inspect key}, es #{inspect value}.")
+          buzon(true)
+        {:ok, :eval_lamb, :map, op1, op2, value} ->
+          IO.puts("El resultado de evaluar la función con #{inspect op1} y #{inspect op2} es #{inspect value}.")
+          buzon(true)
+
+        # Conjuntos
+        {:ok, :in, :map_set, elem, bool} ->
+          if bool do
+            IO.puts("El elemento #{inspect elem}, pertenece al conjunto.")
+          else
+            IO.puts("El elemento #{inspect elem}, no pertenece al conjunto.")
+          end
+          buzon(true)
+        {:ok, :size, :map_set, size} ->
+          IO.puts("El tamaño del conjunto es #{inspect size}.")
+          buzon(true)
+        {:ok, :put, :map_set, elem} ->
+          IO.puts("Se agrego #{inspect elem} al conjunto.")
+          buzon(true)
+
+        # En caso de que la petición sea inválida, devuelve un atómico que
+        # informa del error, el operador que es inválido y la estructura
+        # para la que el operador es inválido.
+        {:error, :op_invalida, op, estruct} ->
+          IO.puts("#{inspect :op_error}: No sé reconoce el operador #{inspect op} para #{inspect estruct}")
+          buzon(true)
+        {:error, :not_found, estruct} ->
+          IO.puts("#{inspect :not_found_error}: El valor introducido no ha sido encontrado en #{inspect estruct}")
+          buzon(true)
+      after
+        1_000 ->
+          "Se rebaso el tiempo de espera."
+          buzon(false)
+      end
+    else
+      "Nada más en el buzón."
     end
   end
 
-  # {:petición, :estructura, remitente, <elementos>}
-  defp server() do
-    l = [1,2,3,4,5]
-    t = {1, :a, 4}
-    m = Map.new([{:a,1},{:b,2}])
-    ms = MapSet.new([1,2,3,4,5,6,7,8,9,1,2,3])
-    receive do
-      {:delete, :list, rem, elem} ->
-        l = List.delete(l, elem)
-        send(rem, {:ok, :delete, :list})
-      {:put, :list, rem, value} ->
-        l = l ++ [value]
-        send(rem, {:ok, :put, :list})
-      {:length, :list, rem} ->
-        length = length(l)
-        send(rem, {:ok, :length, :list, length})
-      {p, :list, rem} ->
-        IO.puts("No se reconoce la petición #{inspect p} para listas")
-        send(rem, {:error, :op_invalida})
-      {p, :list, rem, _} ->
-        IO.puts("No se reconoce la petición #{inspect p} para listas")
-        send(rem, {:error, :op_invalida})
-      {:get, :tuple, rem} ->
-        send(rem, {:ok, :get, :tuple, t})
-      {:set, :tuple, rem, value, i} ->
-        t = Tuple.delete_at(t, i)
-        t = Tuple.insert_at(t, i, value)
-        send(rem, {:ok, :set, :tuple})
-      {:to_list, :tuple, rem} ->
-        tl = Tuple.to_list(t)
-        send(rem, {:ok, :to_list, :tuple, tl})
-      {p, :tuple, rem} ->
-        IO.puts("No se reconoce la petición #{inspect p} para tuplas")
-        send(rem, {:error, :op_invalida})
-      {p, :tuple, rem, _} ->
-        IO.puts("No se reconoce la petición #{inspect p} para tuplas")
-        send(rem, {:error, :op_invalida})
-      {:put, :map, rem, key, elem} ->
-        m = Map.put(m, key, elem)
-        send(rem, {:ok, :put, :map})
-      {:get_value, :map, rem, key} ->
-        v = Map.get(m, key)
-        if v != nil do
-          send(rem, {:ok, :get_value, :map, v})
-        else
-          IO.puts("No se encontro el elemento.")
-          send(rem, {:error, :not_found})
-        end
-      {:eval_lamb, :map, rem, key, fun, op} ->
-        v = Map.get(m, key)
-        res = fun.(v,op)
-        send(rem, {:ok, :eval_lamb, :map, res})
-      {p, :map, rem} ->
-        IO.puts("No se reconoce la petición #{inspect p} para diccionarios.")
-        send(rem, {:error, :op_invalida})
-      {p, :map, rem, _} ->
-        IO.puts("No se reconoce la petición #{inspect p} para diccionarios.")
-        send(rem, {:error, :op_invalida})
-      {:in, :sap_set, rem, elem} ->
-        i = MapSet.member?(ms, elem)
-        send(rem, {:ok, :in, :map_set, i})
-      {:size, :map_set, rem} ->
-        li_map = MapSet.to_list(ms)
-        size = length(li_map)
-        send(rem,{:ok, :size, :map_set, size})
-      {:put, :map_set, rem, elem} ->
-        ms = MapSet.put(ms, elem)
-        send(rem, {:ok, :put, :map_set})
-      {p, :map_set, rem} ->
-        IO.puts("No se reconoce la petición #{inspect p} para conjuntos.")
-        send(rem, {:error, :op_invalida})
-      {p, :map_set, rem, _} ->
-        IO.puts("No se reconoce la petición #{inspect p} para conjuntos.")
-        send(rem, {:error, :op_invalida})
+  @doc """
+    Va a iniciar un servidor donde va a ir recibiendo las estructuras de
+    datos. Esta implementación originalmente contenia las propias estruc-
+    turas. Sin embargo la extensión de funcionalidad, para que el servi-
+    dor se mantuviera escuchando, demandaba que las estructuras fuera re-
+    cibidas en cada llamada recursiva.
+
+    En cuanto al paso de mensajes, el formato de entrada es:
+
+    {:petición, :estructura, remitente, ...}
+
+    La :petición será un atómico que describa la operación que quiere
+    que se realice sobre la estructura de datos, En general estas son:
+
+      : get -> devuelve un elemento o subestructura.
+      : set -> modifica la estrcutura.
+      : length / : size -> devuelve el tamaño de la estructura.
+      : delete -> elimina un elemento o una subestrcutrua.
+
+    Hay múltiples casos partículares, pero se espera que se sobreen-
+    tiendan con el nombre de los atómicos.
+
+    La :estructura, será aquella sobre la que se quiere operar, y
+    pueden ser:
+
+      :list -> lista de elementos.
+      :tuple -> tupla.
+      :map -> diccionario.
+      :map_set -> conjunto.
+
+      Cada estructura es tratada según las partícularidades de la
+      misma.
+
+      Remitente hace referencia al proceso que envia el mensaje,
+      esto con el propósito de poder contestar a dicho mensaje, con
+      la información solicitada
+
+      Finalmente se incluye una serie de paramétros que pueden ser
+      desde 0 a n, según los requerimientos de la operación. Por
+      ejemplo: :lenght para listas no necesita ningún paramétro,
+      mientras que :delete para esta misma estructura, necesita de
+      el elemento que necesita eliminar.
+
+      Mientras que los mensajes de contestación, se comportan de ma-
+      nera similar. Existen principalmente dos tipos:
+
+        :ok -> cuando la operación se pudo realizar correctamente.
+        :error -> cuando algo impidio que se completara la opera-
+        ción
+
+      En el caso de los mensajes satisfactorios, se va a devolver
+      además, la solicitud que se hizo al servidor, el tipo de
+      estructura sobre la que se opero y siempre algún paramétro
+      adicional. Ya sea la entrada, para informar del éxito de la
+      operación al usuarix, o el resultado en caso de que sea par-
+      te de la solicitud, por ejemplo, en los gets.
+
+      Así, el servidor estará leyendo todo su buzón, una vez que no
+      se encuentren mensajes y después de 1 segundo de espera, se
+      desactiva la bandera del servidor y con esto. Se cierra la
+      comunicación.
+  """
+  defp server(l, t, m, ms, bandera) do
+    if bandera do
+      receive do
+        # Borra un elemento de la lista.
+        {:delete, :list, rem, elem} ->
+          l = List.delete(l, elem)
+          # Se uso para vérificar que hubiera eliminado los elementos de la lista.
+          # Enum.each(l, fn x -> IO.puts(x) end)
+          send(rem, {:ok, :delete, :list, elem})
+          server(l, t, m, ms, true)
+
+        # Añade un elemento a la lista.
+        {:add, :list, rem, elem} ->
+          l = l ++ [elem]
+          # Se uso para vérificar que hubiera eliminado los elementos de la lista.
+          # Enum.each(l, fn x -> IO.puts(x) end)
+          send(rem, {:ok, :add, :list, elem})
+          server(l, t, m, ms, true)
+
+        # Devuelve el tamaño de la lista
+        {:length, :list, rem} ->
+          length = length(l)
+          send(rem, {:ok, :length, :list, length})
+          server(l, t, m, ms, true)
+
+        # Se ponen clausulas en caso de que el operador sea incorrecto.
+        {op, :list, rem} ->
+          send(rem, {:error, :op_invalida, op, :list})
+          server(l, t, m, ms, true)
+
+        {op, :list, rem, _} ->
+          send(rem, {:error, :op_invalida, op, :list})
+          server(l, t, m, ms, true)
+
+
+        # Devuelve la tupla.
+        {:get, :tuple, rem} ->
+          send(rem, {:ok, :get, :tuple, t})
+          server(l, t, m, ms, true)
+
+        # Actualiza el valor i de la tupla.
+        {:set, :tuple, rem, value, i} ->
+          t = Tuple.delete_at(t, i)
+          t = Tuple.insert_at(t, i, value)
+          send(rem, {:ok, :set, :tuple, value, i})
+          server(l, t, m, ms, true)
+
+        # Devuelve la tupla como una lista.
+        {:to_list, :tuple, rem} ->
+          t_list = Tuple.to_list(t)
+          send(rem, {:ok, :to_list, :tuple, t_list})
+          server(l, t, m, ms, true)
+
+        # Se ponen clausulas en caso de que el operador sea incorrecto.
+        {op, :tuple, rem} ->
+          send(rem, {:error, :op_invalida, op, :tuple})
+          server(l, t, m, ms, true)
+
+        {op, :tuple, rem, _} ->
+          send(rem, {:error, :op_invalida, op, :tuple})
+          server(l, t, m, ms, true)
+
+
+        # Se agrega un elemento con su llave al diccionario.
+        {:put, :map, rem, key, value} ->
+          m = Map.put(m, key, value)
+          send(rem, {:ok, :put, :map, key, value})
+          server(l, t, m, ms, true)
+
+        # Devuelve el valor de la llave indicada.
+        {:get_value, :map, rem, key} ->
+          value = Map.get(m, key)
+          # Verifica si el valor existe.
+          if value != nil do
+            send(rem, {:ok, :get_value, :map, key, value})
+          else
+            send(rem, {:error, :not_found, :map})
+          end
+          server(l, t, m, ms, true)
+
+        # Evalua la función lambda con el operador introducido y el obtenido
+        # de la llave marcada.
+        {:eval_lamb, :map, rem, key, fun, op} ->
+          value = Map.get(m, key)
+          res = fun.(value,op)
+          send(rem, {:ok, :eval_lamb, :map, value, op, res})
+          server(l, t, m, ms, true)
+
+        # Se ponen clausulas en caso de que el operador sea incorrecto.
+        {op, :map, rem} ->
+          send(rem, {:error, :op_invalida, op, :map})
+          server(l, t, m, ms, true)
+
+        {op, :map, rem, _} ->
+          send(rem, {:error, :op_invalida, op, :map})
+          server(l, t, m, ms, true)
+
+
+        # Verifica que el elemento este en el conjunto.
+        {:in, :map_set, rem, elem} ->
+          bool = MapSet.member?(ms, elem)
+          send(rem, {:ok, :in, :map_set, elem, bool})
+          server(l, t, m, ms, true)
+
+        # Devuelve el tamaño del conjunto.
+        {:size, :map_set, rem} ->
+          m_list = MapSet.to_list(ms)
+          size = length(m_list)
+          send(rem,{:ok, :size, :map_set, size})
+          server(l, t, m, ms, true)
+
+        # Agrega un elmento al conjunto.
+        {:put, :map_set, rem, elem} ->
+          ms = MapSet.put(ms, elem)
+          send(rem, {:ok, :put, :map_set, elem})
+          server(l, t, m, ms, true)
+
+        # Se ponen clausulas en caso de que el operador sea incorrecto.
+        {op, :map_set, rem} ->
+          send(rem, {:error, :op_invalida, op, :map_set})
+          server(l, t, m, ms, true)
+
+        {op, :map_set, rem, _} ->
+          send(rem, {:error, :op_invalida, op, :map_set})
+          server(l, t, m, ms, true)
+      after
+        1_000 ->
+          server(l, t, m, ms, false)
+      end
+    else
+      IO.puts("Buzón del servidor completamente leído.")
     end
   end
 
