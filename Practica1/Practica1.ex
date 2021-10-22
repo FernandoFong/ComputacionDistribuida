804054e1614a27f7810b830981aa1f13a4854956
RealMaldov <amaldov@ciencias.unam.mx>
2021-10-21 23:41:22 -0500

    mi cochinada

--- Practica1/Practica1.ex
+++ Practica1/Practica1.ex
@@ -1,11 +1,29 @@
 defmodule Module1 do
 
   def fibonacci(n) do
-    :ok
+    cond do
+      n < 0 ->
+        "valor no valido"
+      n > 1 ->
+        fibonacci(n - 1) + fibonacci (n - 2)
+      n == 0 ->
+        0
+      n == 1 ->
+        1
+      end
   end
 
   def factorial(n) do
-    :ok
+    cond do
+    n < 0 ->
+      "Valor no valido"
+    n > 1 ->
+      n * factorial(n - 1)
+    n == 1 ->
+      1
+    n == 0 ->
+      1
+    end
   end
 
   def random_probability(n) do
@@ -21,6 +39,39 @@ defmodule Module1 do
 
 end
 
+defmodule Module2 do
+
+  def test () do
+    fn (_) -> {:ok} end
+  end
+
+  def solve(a, b, n) do
+
+    cont = 1
+    uno =1
+
+    Enum.each(1..n, fn(x)->
+      #IO.puts x
+
+      if a * x == n * cont + 1 do
+        IO.puts "son primos" end
+
+      cont = if a * x > n * cont do
+       cont + 1 else cont + 1 end
+       #IO.puts "Contador aumrento:"
+       #IO.puts cont end
+
+       if a * x != n * cont + 1 do
+        IO.puts a * x
+        IO.puts "no es igual a"
+        IO.puts n * cont + 1 end
+
+    end)
+
+  end
+
+end
+
 defmodule Module3 do
 
   def rev(l) do
@@ -32,9 +83,38 @@ defmodule Module3 do
   end
 
   def elim_dup(l) do
-    :ok
+    [head | tail] = l
+    IO.puts head
+    contador = 0
+    lista = []
+
+    Enum.each(l, fn(x)->
+      IO.puts "Elemento:"
+      IO.puts x
+      IO.puts "Tamaño lista: "
+      IO.puts length(lista)
+      if length(lista) == 1 do
+      lista = lista ++ [x]
+      end
+      Enum.each(lista, fn(y)->
+        if x != y do
+          IO.puts x
+          IO.puts "No es igual a: "
+          IO.puts y
+          contador = contador + 1
+        else IO.puts "Elemento repetido" end
+        end)
+
+        if contador == length(lista)do
+          [x |lista]
+          contador = 0
+        end
+
+      end)
+
+      lista
   end
-  
+
 end
 
 defmodule Module4 do
@@ -42,5 +122,5 @@ defmodule Module4 do
   def monstructure() do
     :ok
   end
-  
+
 end
